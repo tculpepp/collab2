32f6adfa8ef29ed63e9555390a4188695745d3e3
tculpep <tculpepp@gmail.com>
2023-05-05 02:49:39 +0000

    reset database for live site

--- collab_project/settings.py
+++ collab_project/settings.py
@@ -118,15 +118,15 @@ WSGI_APPLICATION = 'collab_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
-# DATABASES = {'default': env.db()}
+DATABASES = {'default': env.db()}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
