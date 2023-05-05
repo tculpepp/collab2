e4dd347b3d3132e494539c787299c4193122ee37
tculpep <tculpepp@gmail.com>
2023-05-05 02:48:00 +0000

    added password reset button to login

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,5 +6,5 @@ from .views import SignUpView, ResetPasswordView
 # URL pattern for the account creation page
 urlpatterns = [
     path("signup/", SignUpView.as_view(), name="signup"),
-    path('password-reset/', ResetPasswordView.as_view(), name='password_reset'),
+    path('password-reset/', ResetPasswordView.as_view(), name='password-reset'),
 ]
